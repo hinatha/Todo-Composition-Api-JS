9fddaa5839e909e3335c458a727e425f7c86a350
hinatha <hinatha0110@gmail.com>
2022-11-27 17:41:21 +0900

    todo app実装完了

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todoリスト</h1>
+    <!-- The data in v-model is synchronized with the form input value -->
+    <input v-model="todo"><br>
+    <!-- Call addTodo method by click event -->
+    <button @click="addTodo">追加</button>
+    <ul>
+      <!-- Display todos with index as key -->
+      <!-- Call removeTodo method with index as an argument  -->
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }} <span @click="removeTodo(index)">X</span></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // Set form empty todo and todo list(todos)
+  data() {
+    return {
+      todo: '',
+      todos:[]
+    }
+  },
+  methods: {
+    addTodo() {
+      // Add todo to todos in data()
+      this.todos.push(this.todo)
+      // Change to empty todo in form
+      this.todo = ''
+    },
+    removeTodo(index) {
+      // splice(index: position of starting to remove, 1: the number of removing)
+      this.todos.splice(index, 1)
+    }
   }
 }
 </script>
 
+// non style
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
